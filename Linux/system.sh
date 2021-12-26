4d0accbf6d9236243fe078eb839999f356d3c355
SCARR1515 <89826430+SCARR1515@users.noreply.github.com>
2021-12-26 16:39:19 -0600

    Create system.sh

---
+++ Linux/system.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Free Memory
+free -h > ~/backups/freemem/free_mem.txt
+# diskusage
+du -h > ~/backups/diskuse/disk_use.txt
+# open files
+lsof > ~/backups/openlist/open_list.txt
+# diskspace
+df -h > ~/backups/freedisk/free_disk.txt
+© 2021 GitHub, Inc.
+Terms
+Privacy
